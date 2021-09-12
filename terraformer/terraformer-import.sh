a98ef3ac962193064d592d316cadcebdb4605613
angliwen <85338632+angliwen@users.noreply.github.com>
2021-09-13 00:24:39 +0800

    Update and rename terraformer-import_US.sh to terraformer-import.sh

--- terraformer/terraformer-import.sh
+++ terraformer/terraformer-import.sh
@@ -17,11 +17,13 @@ sudo mv terraformer-${PROVIDER}-linux-amd64 /usr/local/bin/terraformer
 
 #
 
-gcloud iam service-accounts keys create group1-sa-key-file \
---iam-account=group1@group1-6m11.iam.gserviceaccount.com
+. ~/gcp/config-xonotic-env.sh
 
-export GOOGLE_APPLICATION_CREDENTIALS=group1-sa-key-file # <file path of the JSON file that contains our service account key>
+gcloud iam service-accounts keys create sa-key-file \
+--iam-account=$SERVICE_ACCOUNT_ID@$PROJECT_ID.iam.gserviceaccount.com
 
-terraformer import google --regions=us-central1 --projects=group1-6m11 -v
+export GOOGLE_APPLICATION_CREDENTIALS=sa-key-file # <file path of the JSON file that contains our service account key>
 
-cd generated/google/group1-6m11/
+terraformer import google --regions=asia-southeast1 --projects=$PROJECT_ID -v
+
+cd generated/google/$PROJECT_ID/
